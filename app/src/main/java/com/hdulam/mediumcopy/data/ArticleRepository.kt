6c180a267b82bf3e7fddcf883cec14b9c76f8307
Héctor Duarte <50688196+hdulam@users.noreply.github.com>
2026-08-13 12:35:04 -0600

    article repository carnet terminacion 9

--- app/src/main/java/com/hdulam/mediumcopy/data/ArticleRepository.kt
+++ app/src/main/java/com/hdulam/mediumcopy/data/ArticleRepository.kt
@@ -6,24 +6,24 @@ object ArticleRepository {
     fun getList() = listOf<Article>(
         Article(
             name = "Juan Lopez",
-            title = "Lorem ipsum",
-            resume = "Lorem impsum resume",
+            title = "Ver peliculas durante el entrenamiento es bueno",
+            resume = "Ha habido un boom en los gimnasios que ponen peliculas en las salas de cardio, descubre por qué.",
             readTime = "23",
-            date = "11 dic"
+            date = "11 ene"
         ),
         Article(
-            name = "Juan Lopez",
-            title = "Lorem ipsum",
-            resume = "Lorem impsum resume",
+            name = "Jonathan Lopez",
+            title = "El ajedrez deja de ser considerado un deporte",
+            resume = "Según la nueva clasificación de la ONU, el ajedrez ya no es un deporte",
             readTime = "23",
-            date = "11 dic"
+            date = "22 dic"
         ),
         Article(
-            name = "Juan Lopez",
-            title = "Lorem ipsum",
-            resume = "Lorem impsum resume",
-            readTime = "23",
-            date = "11 dic"
+            name = "Carlos Cardona",
+            title = "Se inventan un nuevo deporte",
+            resume = "¿Una combinacion de varios existentes o uno completamente nuevo? Esto es aún un debate abierto.",
+            readTime = "29",
+            date = "11 feb"
         )
     )
 
